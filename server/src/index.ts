4a6b9b20cfa8b09199ce73266589233f36b21a0e
Batin EVIRGEN <batinevirgen@gmail.com>
2022-06-26 13:09:45 +0300

    add basic express server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello, world!");
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import helmet from "helmet";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 8080;
+const app: Express = express();
+
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello, world!");
+});
+
+app.listen(PORT, () => {
+  console.log(
+    `Server is running on 'http://127.0.0.1:${PORT}' (or 'http://localhost:${PORT}')`
+  );
+});
